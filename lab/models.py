63f5af1ab5558320c929f5affea19aaf076eb0e3
Hamsa Abdirashid <hamzsaabdi@gmail.com>
2023-12-09 09:36:11 +0300

    add home , navbar, detail page design

--- lab/models.py
+++ lab/models.py
@@ -1,29 +1,38 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.conf import settings
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.category_name
+    
 
 
 class Book(models.Model):
     author = models.CharField(max_length=50, null=False)
-    book_title = models.CharField(max_length=200, null=False)
+    title = models.CharField(max_length=200, null=False)
     category = models.ForeignKey(Category , on_delete=models.CASCADE)
-    publication_date = models.DateField()
-    cover_image = models.ImageField(upload_to='book_covers', max_length=200, blank=True)
-    pdf_book = models.FileField(upload_to='pdf_books', max_length=200, blank=True)
+    summary = models.TextField(blank=True)
+    published_at = models.DateField()
+    cover = models.ImageField(upload_to='book_covers', max_length=200, blank=True)
+    src_pdf = models.FileField(upload_to='pdf_books', max_length=200, blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.author
+        return f"{self.author} - {self.title}"
+    
+    def src_file_type(self):
+        filename = self.src_pdf.name.split(".")
+        return filename[-1]
 
-class School(models.Model):
-    school_name = models.CharField(max_length=50)
+class Collection(models.Model):
+    owner = models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
+    books  = models.ManyToManyField(Book, related_name="collections", blank=True)
 
     def __str__(self):
-        return self.school_name
-
-class Logo(models.Model):
-    atu_logo = models.ImageField(upload_to='atu_logo', blank=True)
+        return f"{self.owner.username}'s collection"
--- lab/views.py
+++ lab/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from django.http import HttpResponse
 from .models import Book
-from .forms import CategoryForm, BookForm , SchoolForm
-from .models import Category, Book , School , Logo
+from .forms import CategoryForm, BookForm 
+from .models import Category, Book
 from.filters import  BookFilter
+import random
+import mimetypes
+
+def home(request):
+    # categories = Category.objects.all()
+    # books = Book.objects.all()
+    # random_books_by_category = {cat:random.choice(books.filter(category = cat)) for cat in categories}
+
+    # context = {
+    #     "books": Book.objects.all()
+    # }
+
+    books = Book.objects.all()
+    
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'lab/home.html', context)
+
+def download_book(request, book):
+    try:
+        book = get_object_or_404(Book, id=book)
+        path = book.src_pdf.path
+        file = open(path, 'r')
+        filename = f"{book.title.replace(' ', '_')}.{book.src_file_type()}"
+        response = HttpResponse(file, content=mimetypes.guess_type(filename))
+        response['Content-Disposition'] = f"attachment; filename={filename}"
+        return response    
+    except:
+        return render(request, "lab/404.html")
+    
 
 def create_category(request):
     if request.method == 'POST':
@@ -16,18 +48,6 @@ def create_category(request):
 
     return render(request, 'lab/create_category.html', {'form': form})
 
-
-def create_school(request):
-    if request.method == 'POST':
-        form = SchoolForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book-list')
-    else:
-        form = SchoolForm()
-
-    return render(request, 'lab/create_school.html', {'form': form})
-
 def create_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
@@ -39,16 +59,16 @@ def create_book(request):
     return render(request, 'lab/create_book.html', {'form': form})
        
 
-def home(request):
-    book_filter = BookFilter(request.GET, queryset=Book.objects.all())
-    books = book_filter.qs
-    logo = Logo.objects.all()
-    context = {
-        'books': books,
-        'logo': logo,
-        'book_filter': book_filter,
-    }
-    return render(request, 'lab/list.html', context)
+# def home(request):
+#     book_filter = BookFilter(request.GET, queryset=Book.objects.all())
+#     books = book_filter.qs
+#     logo = Logo.objects.all()
+#     context = {
+#         'books': books,
+#         'logo': logo,
+#         'book_filter': book_filter,
+#     }
+#     return render(request, 'lab/list.html', context)
 
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
